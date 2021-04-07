30c7a21b99adb9243115c5b8d622fe7a33649521
Camilo Rincon <camilo.rincon@tambourine.com>
2021-04-07 08:11:56 -0500

    Add models

---
+++ models/review.js
@@ -0,0 +1,15 @@
+const mongoose = require ('mongoose')
+//const passportLocalMongoose = require('passport-local-mongoose')
+const Schema = mongoose.Schema
+
+const ReviewSchema = new Schema({
+    body : String,
+    author : {
+        type : Schema.Types.ObjectId,
+        ref : 'User'
+    }
+});
+
+//ReviewSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model('Review',ReviewSchema)
