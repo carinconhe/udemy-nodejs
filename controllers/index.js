955a9b3491a5c54df289a3274aa52bb3379c3988
Camilo Rincon <camilo.rincon@tambourine.com>
2021-04-12 17:47:47 -0500

    add middlaware handle erros, passport library and session login

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,27 @@
 const User = require('../models/user')
+const passport = require('passport');
 
 module.exports = {
-    postRegister(req,res,next){
-        
+    //postRegister method
+    async postRegister(req,res,next){
         const newUser = new User({
             username:req.body.username,
             email:req.body.email,
             image:req.body.image
         })
-        
-        User.register(newUser, req.body.password, function(err) {
-            if (err) {
-            console.log('error while user register!', err);
-            return next(err);
-            }
-
-            console.log('user registered!');
-
-            res.redirect('/');
-        });
+        await User.register(newUser, req.body.password);
+        res.redirect('/');
+    },
+    //Post /login
+    postLogin(req,res,next){
+        passport.authenticate('local',{ 
+            successRedirect: '/',
+            failureRedirect: '/login' 
+        })(req,res,next);
+    },
+    //GEt /logout
+    getLogout(req,res,next){
+        req.logout();
+        res.redirect('/');  
     }
 }
