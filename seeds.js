8dacd0997cfbc31e334987118411abb417005bfb
Camilo Rincon <camilo.rincon@tambourine.com>
2021-06-09 16:07:54 -0500

    fixed bug with url params

--- seeds.js
+++ seeds.js
@@ -16,10 +16,7 @@ async function seedPosts() {
 				type: 'Point',
 				coordinates: [cities[random1000].longitude, cities[random1000].latitude],
 			},
-			author: {
-		    '_id' : '609537eb8ddeaaa801d6e362',
-		    'username' : 'Camilo'
-		  }
+			author: '609537eb8ddeaaa801d6e362'
 		}
 		let post = new Post(postData);
 		post.properties.description = `<strong><a href="/posts/${post._id}">${title}</a></strong><p>${post.location}</p><p>${description.substring(0, 20)}...</p>`;
