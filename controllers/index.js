2264cc75776363614d362ceef5b5efe9adee1e87
Camilo Rincon <camilo.rincon@tambourine.com>
2021-06-29 08:05:41 -0500

    Profile access, change password and update profle

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const mapBoxToken = process.env.MAPBOX_TOKEN;
+const util = require('util');
 const User = require('../models/user')
 const Post = require('../models/post')
 const passport = require('passport');
@@ -54,5 +55,31 @@ module.exports = {
 	getLogout(req, res, next) {
 	  req.logout();
 	  res.redirect('/');
+	},
+	// GET /profile
+	async getProfile(req, res, next) {
+	  const posts = await Post.find().where('author').equals(req.user._id).limit(10).exec();
+	  res.render('profile', { posts });
+	},
+	async updateProfile(req, res, next) {
+		// destructure username and email from req.body
+		const {
+			username,
+			email
+		} = req.body;
+		// destructure user object from res.locals
+		const { user } = res.locals;
+		// check if username or email need to be updated
+		if (username) user.username = username;
+		if (email) user.email = email;
+		// save the updated user to the database
+		await user.save();
+		// promsify req.login
+		const login = util.promisify(req.login.bind(req));
+		// log the user back in with new info
+		await login(user);
+		// redirect to /profile with a success flash message
+		req.session.success = 'Profile successfully updated!';
+		res.redirect('/profile');
 	}
 }
