0e207f067b51e2ba07872273bf7f4c314c24dfb6
Camilo Rincon <camilo.rincon@tambourine.com>
2021-08-18 15:41:20 -0500

    add theming to my project

--- seeds.js
+++ seeds.js
@@ -19,7 +19,12 @@ async function seedPosts() {
 			},
 			price: random1000,
 			avgRating: random5,
-			author: '609537eb8ddeaaa801d6e362'
+			author: '609537eb8ddeaaa801d6e362', // replace with your user id
+			images: [
+				{
+				url: 'https://res.cloudinary.com/devsprout/image/upload/v1561315599/surf-shop/surfboard.jpg'
+				}
+			]
 		}
 		let post = new Post(postData);
 		post.properties.description = `<strong><a href="/posts/${post._id}">${title}</a></strong><p>${post.location}</p><p>${description.substring(0, 20)}...</p>`;
